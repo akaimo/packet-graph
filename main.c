471dc8ac0d41ce037e27dacaaf043e09a4202f01
shuhei <kawashu0723@gmail.com>
2018-11-28 22:33:23 +0900

    ether header

--- main.c
+++ main.c
@@ -4,6 +4,9 @@
 
 #include <stdio.h>
 #include <pcap.h>
+#include <netinet/if_ether.h>
+#include <arpa/inet.h>
+#include <string.h>
 
 #define DPCP_RCV_MAXSIZE   68
 #define DPCP_PROMSCS_MODE  1
@@ -11,6 +14,8 @@
 #define DPCP_NOLIMIT_LOOP  -1
 
 void start_pktfunc(u_char *, const struct pcap_pkthdr *, const u_char *);
+
+char *convmac_tostr(u_char *, char *, size_t);
 
 int main() {
     pcap_t *pd = NULL;
@@ -29,5 +34,19 @@ int main() {
 }
 
 void start_pktfunc(u_char *user, const struct pcap_pkthdr *h, const u_char *p) {
-    printf("receive packet\n");
+    char dmac[18] = {0};
+    char smac[18] = {0};
+    struct ether_header *eth_hdr = (struct ether_header *) p;
+
+    printf("ether header---------\n");
+    printf("dest mac %s\n", convmac_tostr(eth_hdr->ether_dhost, dmac, sizeof(dmac)));
+    printf("src mac %s\n", convmac_tostr(eth_hdr->ether_shost, smac, sizeof(smac)));
+    printf("ether type %x\n\n", ntohs(eth_hdr->ether_type));
+}
+
+char *convmac_tostr(u_char *hwaddr, char *mac, size_t size) {
+    snprintf(mac, size, "%02x:%02x:%02x:%02x:%02x:%02x",
+             hwaddr[0], hwaddr[1], hwaddr[2],
+             hwaddr[3], hwaddr[4], hwaddr[5]);
+    return mac;
 }
